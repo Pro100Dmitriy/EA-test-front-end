2f738958072c04c0ad808d58ba09d5328a3afe62
Dmitry Shestak <dimonshestak160201@gmail.com>
2022-03-13 13:41:22 +0300

    popup added

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,28 +86,57 @@ class Timer{
     }
 }
 
-document.addEventListener( 'DOMContentLoaded', () => {
+const popupModule = () => {
+    const pupup = document.querySelector('#popup')
+    const message = popup.querySelector('.message')
+    const closeButtonAll = pupup.querySelectorAll('#close-popup')
 
+    closeButtonAll.forEach( item => item.addEventListener( 'click', () => {
+        message.classList.remove('message-open')
+        setTimeout( () => { pupup.classList.remove('popup-open') }, 200 )
+    } ) )
 
-const timer = new Timer({
-    initialDate: 'Dec 31 2022 00:00:00',
-    timerId: 'timer'
-})
+    return {
+        open(){
+            pupup.classList.add('popup-open')
+            setTimeout( () => { message.classList.add('message-open') }, 200 )
+        }
+    }
+}
 
+const isEmail = ( initialValue ) => {
+    const regExEmail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/g
+    const testEmail = initialValue.match( regExEmail )
 
-const subscribeForm = document.getElementById('subscribe')
-
-subscribeForm.addEventListener( 'submit', subscribe )
-
-function subscribe( event ){
-    event.preventDefault()
-
-    const email = event.target[0].minutesValue 
-
-    if( email !== '' ){
-        request( '', 'POST', {email} )
+    if( testEmail ){
+        return true
     }else{
-        console.log( 'Заполните поле' )
+        return false
+    }
+}
+
+const formModul = () => {
+    const subscribeForm = document.querySelector('#subscribe')
+    const error = subscribeForm.querySelector('.subscribe__errors')
+    const input = subscribeForm.querySelector('.subscribe__input')
+    const button = subscribeForm.querySelector('.subscribe__submit')
+    return {
+        form: subscribeForm,
+        inputClear(){
+            input.value = ''
+        },
+        disable(){
+            button.disabled = true
+        },
+        enable(){
+            button.disabled = false
+        },
+        showError(){
+            error.classList.remove('err-hidden')
+        },
+        hideError(){
+            error.classList.add('err-hidden')
+        }
     }
 }
 
@@ -121,5 +150,39 @@ async function request( url, method = 'GET', body = null, headers = {'Content-Ty
         console.log( error )
     }
 }
+
+
+
+document.addEventListener( 'DOMContentLoaded', () => {
+
+    /** Timer  */
+    const timer = new Timer({
+        initialDate: 'Dec 31 2022 00:00:00',
+        timerId: 'timer'
+    })
+
+    /** Subscribe  */
+    const subscribeForm = formModul()
+    const popup = popupModule()
+
+    subscribeForm.form.addEventListener( 'submit', subscribe )
+
+    function subscribe( event ){
+        event.preventDefault()
+
+        const email = event.target[0].value
+        
+        if( isEmail( email ) ){
+            popup.open()
+        }else{
+            subscribeForm.inputClear()
+            subscribeForm.disable()
+            subscribeForm.showError()
+            setTimeout( () => {
+                subscribeForm.enable()
+                subscribeForm.hideError()
+            }, 5000 )
+        }
+    }
 
 } )
