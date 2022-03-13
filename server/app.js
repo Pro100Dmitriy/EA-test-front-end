cff7bdb54b5e092bf2e1b3a0c5f81f9e97e76d8b
Dmitry Shestak <dimonshestak160201@gmail.com>
2022-03-14 00:38:17 +0300

    upload to heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const cors = require('cors')
 
 const route = require('./routes/subsribe')
 const keys = require('./config/keys')
@@ -15,9 +14,6 @@ mongoose.connect( keys.mongoURI )
 
 app.use( bodyParser.urlencoded({extended: true}) )
 app.use( bodyParser.json() )
-// app.use( cosr({
-//     e
-// }) )
 
 app.use( '/api', route )
 
